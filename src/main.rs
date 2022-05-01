bbb52a7100ca487d4e69d5244298c8ce29e95661
Guido Giuntoli <gagiuntoli@gmail.com>
2022-05-01 02:21:04 +0200

    Implementing the cacher with Generics

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
 use std::{collections::HashMap};
+use std::hash::Hash;
 use std::thread;
 use std::time::Duration;
 
-
-struct Cacher<T>
-    where T: Fn(u32) -> u32
+struct Cacher<T,V,K>
+    where T: Fn(V) -> K
 {
     calculation: T,
-    data: HashMap<u32,u32>,
+    data: HashMap<V,K>,
 }
 
-impl<T> Cacher<T>
-    where T: Fn(u32) -> u32
+impl<T,V,K> Cacher<T,V,K>
+    where T: Fn(V) -> K, V: Eq, V: Hash, V: Clone, K: Clone
 {
-    fn new(calculation: T) -> Cacher<T> {
+    fn new(calculation: T) -> Cacher<T,V,K> {
         Cacher {
             calculation,
             data: HashMap::new()
         }
     }
 
-    fn value(&mut self, arg: u32) -> u32 {
+    fn value(&mut self, arg: V) -> K {
         match self.data.get(&arg) {
             Some(result) => result.clone(),
             None => {
-                let newVal = (self.calculation)(arg);
-                self.data.insert(arg, newVal);
+                let newVal = (self.calculation)(arg.clone());
+                self.data.insert(arg, newVal.clone());
                 newVal
             }
         }
@@ -41,6 +41,34 @@ fn main() {
     let mut cacher = Cacher::new(expensive_closure);
     println!("result = {}", cacher.value(34));
     println!("result = {}", cacher.value(34));
+    println!("result = {}", cacher.value(35));
+    println!("result = {}", cacher.value(34));
+
+    let expensive_closure_string = |num| {
+        println!("Calculating slowly ...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+    let mut cacher = Cacher::new(expensive_closure_string);
+    println!("result = {}", cacher.value(String::from("Guido")));
+    println!("result = {}", cacher.value(String::from("Guido")));
+    println!("result = {}", cacher.value(String::from("Guido")));
+    println!("result = {}", cacher.value(String::from("Guido")));
+    println!("result = {}", cacher.value(String::from("Jorge")));
+    println!("result = {}", cacher.value(String::from("Jorge")));
+    println!("result = {}", cacher.value(String::from("Guido")));
+    println!("result = {}", cacher.value(String::from("Guido")));
+
+    let expensive_closure_int_to_string = |num: u32| {
+        println!("Calculating slowly ...");
+        thread::sleep(Duration::from_secs(2));
+        format!("{} signed", num.to_string())
+    };
+    let mut cacher = Cacher::new(expensive_closure_int_to_string);
+    println!("result = {}", cacher.value(34));
+    println!("result = {}", cacher.value(34));
+    println!("result = {}", cacher.value(34));
+    println!("result = {}", cacher.value(35));
     println!("result = {}", cacher.value(35));
     println!("result = {}", cacher.value(34));
 }
